5c6739e30879f5d0644bace07e2569e6c0079751
David <umarov.davidka2004@mail.ru>
2021-11-23 14:39:45 +0500

    add kurs and aboutUs html

--- script.js
+++ script.js
@@ -1,48 +1,98 @@
-$('.lesson__content-slider').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
+
+$('.about__us-slider').slick({
+  centerMode: true,
+  centerPadding: '100px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
-
-  const header = document.querySelector('.header__head')
-  let prevScrollPos = window.pageYOffset;
-
-  window.addEventListener('scroll', () => {
-    let currentscrollPos = window.pageYOffset
-    if(prevScrollPos > currentscrollPos) {
-      header.style.top = '0'
-    }else {
-      header.style.top = '-86px'
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
     }
-    prevScrollPos = currentscrollPos
-  })
+  ]
+});
+
+
+
+
+
+$('.lesson__content-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  // autoplay: true,
+  mouseonhover: true,
+  responsive: [{
+      breakpoint: 1472,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+const header = document.querySelector('.header__head')
+let prevScrollPos = window.pageYOffset;
+
+window.addEventListener('scroll', () => {
+  let currentscrollPos = window.pageYOffset
+  if (prevScrollPos > currentscrollPos) {
+    header.style.top = '0'
+  } else {
+    header.style.top = '-86px'
+  }
+  prevScrollPos = currentscrollPos
+})
+
+const burger = document.querySelector('.header__burger')
+const list = document.querySelector('.header__content-list')
+const closeIcon = document.querySelector('.header__close')
+const head = document.querySelector('.header__head')
+
+burger.addEventListener('click', () => {
+  list.classList.add('active')
+  head.classList.add('active')
+})
+
+closeIcon.addEventListener('click', () => {
+  list.classList.remove('active')
+  head.classList.remove('active')
+})
+
+
+
